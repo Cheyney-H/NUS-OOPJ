8f2ef5e79f00bf72cd435b74986e443eea7fe304
Cheyney-H <2519279825@qq.com>
2025-08-05 14:21:43 +0800

    Update TestPayroll.java

--- MyWorkshop/Payroll/TestPayroll.java
+++ MyWorkshop/Payroll/TestPayroll.java
@@ -1,30 +1,25 @@
 public class TestPayroll{
     public static void main(String[] args){
         Employee[] employees = new Employee[]{
-            new SalariedEmployee("Alice", 1200),
-            new HourlyEmployee("Bob", 70, 10),
-            new ContractEmployee("Charlie", 2000)
+            new SalariedEmployee("Alice", 1200, 100),
+            new HourlyEmployee("Bob", 70, 10, -50),
+            new ContractEmployee("Charlie", 2000, 0)
         };
 
-        double[] adjustments ={100, -50, 0};
-        Bonus bonus = new Bonus(adjustments);
         double totalPayroll = 0;
 
-        for (int i = 0; i < employees.length; i++){
-            Employee employee = employees[i];
-            double adjustment = bonus.getAdjustment(i);
-            //double adjustment = adjustments[i];
-            double finalPay = employee.adjustedPay(adjustment);
+        for (Employee employee : employees){
+            double finalPay = employee.adjustedPay();
 
             System.out.println(employee.toString());
             System.out.printf("Base Pay: $%.1f\n", employee.earnings());
 
-            if (adjustment > 0){
-                System.out.printf("Adjustment: +$%.1f%n", adjustment);
-            }else if(adjustment == 0){
+            if (employee.getBod() > 0){
+                System.out.printf("Adjustment: +$%.1f%n", employee.getBod());
+            }else if(employee.getBod() == 0){
                 System.out.println("Adjustment: $0.0");
             }else{
-                System.out.printf("Adjustment: -$%.1f%n", -adjustment);
+                System.out.printf("Adjustment: -$%.1f%n", -employee.getBod());
             }
 
             System.out.printf("Final Pay: $%.1f\n", finalPay);
@@ -34,4 +29,5 @@ public class TestPayroll{
         }
         System.out.printf("Total Payroll: $%.1f%n", totalPayroll);
     }
+
 }
